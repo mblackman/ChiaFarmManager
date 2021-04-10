ef48b9dd83eef12ad54d9d92a94226291eed51e5
Matt Blackman <mateoblackman@live.com>
2021-04-10 10:15:24 -0600

    Turned program into console app.

---
+++ ChiaFarmManager/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using ChiaAdapter;
+
+namespace ChiaFarmManager
+{
+    public static class Utils
+    {
+        private static readonly string FarmSummaryFormat =
+            "Status: {0}" + Environment.NewLine
+            + "Total Chia Farmed: {1}" + Environment.NewLine
+            + "Plot Count: {2}" + Environment.NewLine
+            + "Total Size of Plots (GiB): {3}" + Environment.NewLine
+            + "Estimated Network Space (GiB): {4}" + Environment.NewLine
+            + "Expected Time to Win: {5}" + Environment.NewLine;
+        public static string ToDisplayString<T>(T value) => value switch
+        {
+            FarmSummary farmSummary => string.Format(FarmSummaryFormat, farmSummary.FarmStatus, farmSummary.TotalChiaFarmed, farmSummary.PlotCount, farmSummary.TotalSizeOfPlotsGiB, farmSummary.EstimatedNetworkSpaceGiB, farmSummary.ExpectedTimeToWin),
+            _ => value.ToString()
+        };
+    }
+}
